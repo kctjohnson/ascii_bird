9562391363bc40482bae7e60cb657e8a25103cf7
kctjohnson <killeanjohnson06@gmail.com>
2021-09-26 18:03:36 -0400

    fixed external globals and added wall code

--- Headers/objects.h
+++ Headers/objects.h
@@ -2,6 +2,9 @@
 
 #include "vec.h"
 
+extern const double scale;
+extern const double speed_cap;
+
 class Collidable {
 public:
     vec2d m_pos, m_size;
@@ -9,12 +12,14 @@ public:
     // Returns true for collide, false if not
     bool CheckCollision(Collidable c);
 
-    virtual void Draw();
-    virtual void Update();
+    void Draw() {}
+    void Update() {}
 };
 
 class Wall : Collidable {
 public:
+    Wall(vec2d start, vec2d size);
+
     void Draw();
     void Update();
 };
--- Source/objects.cpp
+++ Source/objects.cpp
@@ -1,9 +1,6 @@
 #include "../Headers/objects.h"
 #include "../Headers/draw.h"
 
-extern double scale;
-extern double speed_cap;
-
 bool Collidable::CheckCollision(Collidable c) {
     return (c.m_pos.x >= this->m_pos.x &&
             c.m_pos.x <= this->m_pos.x + this->m_size.x &&
@@ -11,12 +8,17 @@ bool Collidable::CheckCollision(Collidable c) {
             c.m_pos.y <= this->m_pos.y + this->m_size.y);
 }
 
-void Wall::Draw() {
+Wall::Wall(vec2d start, vec2d size) {
+    m_pos.set(start);
+    m_size.set(size);
+}
 
+void Wall::Draw() {
+    Rect(m_pos.x, m_pos.y, m_size.x, m_size.y);
 }
 
 void Wall::Update() {
-
+    m_pos.sub(speed_cap / 6.0, 0);
 }
 
 Bird::Bird(vec2d start) {
@@ -25,7 +27,7 @@ Bird::Bird(vec2d start) {
 }
 
 void Bird::Flap() {
-    m_vel.set(0, speed_cap);
+    m_vel.set(0, -speed_cap);
 }
 
 void Bird::Draw() {
@@ -35,7 +37,7 @@ void Bird::Draw() {
 }
 
 void Bird::Update() {
-    vec2d gravity(0, -1);
+    vec2d gravity(0, 0.3);
     m_vel.add(gravity);
     m_vel.limit(speed_cap);
 
