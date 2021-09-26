476a5ec0994ecc94f775b789540eef8c04bfc25e
kctjohnson <killeanjohnson06@gmail.com>
2021-09-26 18:03:52 -0400

    lowered scale and added input handling

--- main.cpp
+++ main.cpp
@@ -3,10 +3,24 @@
 #include "Headers/clock.h"
 #include "Headers/draw.h"
 #include "Headers/vec.h"
+#include "Headers/objects.h"
 
 const double fps_cap = 60;
-const double scale = 40;
+const double scale = 20;
 const double speed_cap = 10;
+
+bool HandleInput(Bird& b) {
+    char c = getch();
+    switch (c) {
+        case 'q':
+            return false;
+
+        case ' ':
+            b.Flap();
+            break;
+    }
+    return true;
+}
 
 int main() {
     // Initialize all of the needed bits for curses
@@ -19,15 +33,22 @@ int main() {
     curs_set(0);
 
     // Our sweet new bird object
-    Bird b;
+    Bird b(vec2d(100, 50));
+    Wall w(vec2d(300, 0), vec2d(10, 100));
 
     // Small clock object to cap the fps
     Clock timer;
     timer.Start();
 
-    while (true) {
+    while (HandleInput(b)) {
         // Clear the beginning screen
         clear();
+
+        w.Update();
+        w.Draw();
+
+        b.Update();
+        b.Draw();
 
         // Write the changes to the console
         refresh();
@@ -37,7 +58,6 @@ int main() {
     }
 
     // Pause for input before cleaning up and exiting
-    getch();
     endwin();
 
     return 0;
