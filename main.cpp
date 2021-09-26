a3fab44a4795a9738648fdd489cd85c80c9c7a5b
kctjohnson <killeanjohnson06@gmail.com>
2021-09-26 17:41:14 -0400

    added some useful globals (oh no!), as well as a bird object

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <curses.h>
 
 #include "Headers/clock.h"
+#include "Headers/draw.h"
+#include "Headers/vec.h"
 
 const double fps_cap = 60;
+const double scale = 40;
+const double speed_cap = 10;
 
 int main() {
     // Initialize all of the needed bits for curses
@@ -13,6 +17,9 @@ int main() {
 
     // No pesky cursor randomly showing up!
     curs_set(0);
+
+    // Our sweet new bird object
+    Bird b;
 
     // Small clock object to cap the fps
     Clock timer;
